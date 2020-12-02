e48f83f88436fa0a892eee1e92c14ea4ffdbcc00
Rex Roof <rexroof@gmail.com>
2020-12-01 21:31:13 -0500

    lots of work, simple answer!

--- day1/main.go
+++ day1/main.go
@@ -1,28 +1,44 @@
 package main
+
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-  inputFile := "input.txt"
+	inputFile := "input.txt"
+	var nums []int
 
-  file, err := os.Open(inputFile)
-  if err != nil { 
-    fmt.Printf("%s", err)
-    os.Exit(1) 
-  }
-  defer file.Close()
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-  var lines []string
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
-  
-  for i, line := range lines {
-    fmt.Println(i, line)
-  }
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	for _, line := range lines {
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		} else {
+			nums = append(nums, i)
+		}
+	}
+
+	for _, x := range nums {
+		for _, y := range nums {
+			if x+y == 2020 {
+				fmt.Printf("%d + %d = 2020\n", x, y)
+			}
+		}
+	}
 }
