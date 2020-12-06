401930b1b528235de5280037722c8b8d350ed3bb
Rex Roof <rexroof@gmail.com>
2020-12-06 15:25:30 -0500

    reworked hash map to count unique answers and where everyone answered

--- day6/main.go
+++ day6/main.go
@@ -6,16 +6,30 @@ import (
 	"os"
 )
 
-func countAndPurge(m map[rune]int) (int, map[rune]int) {
-
-	toats := 0
+func peonPurge(m map[rune]int) map[rune]int {
 	for k := range m {
-		toats += m[k]
 		delete(m, k)
 	}
+	return m
+}
 
-	return toats, m
+func countKeys(m map[rune]int) int {
+	toats := 0
+	for range m {
+		toats++
+	}
+	return toats
+}
 
+func countOnlyAll(count int, m map[rune]int) int {
+	toats := 0
+	for k := range m {
+		// fmt.Printf(" m[%c] = %d (count is %d) \n", k, m[k], count)
+		if m[k] == count {
+			toats++
+		}
+	}
+	return toats
 }
 
 func main() {
@@ -27,29 +41,34 @@ func main() {
 	}
 	defer file.Close()
 
-	grand_total := 0
+	any_answer_total := 0
+	all_answer_total := 0
 	peons := make(map[rune]int)
 	scanner := bufio.NewScanner(file)
+	numPeeps := 0
 	for scanner.Scan() {
 		text := scanner.Text()
 
 		if len(text) > 0 {
 			// fmt.Println(text)
+			numPeeps++
 			for _, char := range text {
-				peons[char] = 1
+				peons[char]++
 				// fmt.Println(peons)
 			}
 		} else {
 			// found blank line.  calc totals and reset map
-			var toats int
-			toats, peons = countAndPurge(peons)
-			//fmt.Printf("our total was %d\n", toats)
-			grand_total += toats
+			any_answer_total += countKeys(peons)
+			all_answer_total += countOnlyAll(numPeeps, peons)
+
+			numPeeps = 0
+			peons = peonPurge(peons)
 		}
 	}
-	var toats int
-	toats, peons = countAndPurge(peons)
-	//fmt.Printf("our total was %d\n", toats)
-	grand_total += toats
-	fmt.Printf("grand total: %d\n", grand_total)
+	// count last object
+	any_answer_total += countKeys(peons)
+	all_answer_total += countOnlyAll(numPeeps, peons)
+
+	fmt.Printf("any answer total: %d\n", any_answer_total)
+	fmt.Printf("all answer total: %d\n", all_answer_total)
 }
