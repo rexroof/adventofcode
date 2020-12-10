2bcee906952bd533c031f1f6517fffd12b5fb821
Rex Roof <rexroof@gmail.com>
2020-12-10 10:26:42 -0500

    tried a method where we "fix" the most recent nop/jmp that caused us to loop

--- day8/main.go
+++ day8/main.go
@@ -45,16 +45,60 @@ func main() {
 	}
 	// acc , increase accumulator
 
+	// quick sanity check.. do any nop operations contain an argument that would get us to the end?
+	/* wasnt' this easy
+	for x, ins := range instructionsList {
+		if ins.Operation == "nop" {
+			if (x + ins.Argument) == len(instructionsList) {
+				fmt.Println("found one!")
+				fmt.Println(x)
+				fmt.Println(ins)
+			}
+		}
+	}
+	fmt.Println("exited")
+	os.Exit(0)
+	*/
+
 	accumulator := 0
 	index := 0
+	var stack [][]int
 process:
 	for {
+		// if our index is beyond our instructions, we're done
+		if index >= len(instructionsList) {
+			break process
+		}
 		current := instructionsList[index]
-		//fmt.Printf("acc %d idx %d op %s arg %d exec %d \n",
-		//   accumulator, index, current.Operation, current.Argument, current.Executed)
+		fmt.Printf("acc %d idx %d op %s arg %d exec %d \n",
+			accumulator, index, current.Operation, current.Argument, current.Executed)
+		stack = append(stack, []int{accumulator, index})
+		fmt.Println(stack[len(stack)-1])
 
 		if current.Executed > 0 {
-			break process
+			fmt.Printf("hit repeat.  accumulator is %d\n", accumulator)
+			// if we've already executed this, rewind using the stack
+
+			rewind := stack[len(stack)-2]
+			fmt.Printf("rewound stack contains: acc %d idx %d op %s arg %d exec %d \n",
+				rewind[0], rewind[1], instructionsList[rewind[1]].Operation,
+				instructionsList[rewind[1]].Argument, instructionsList[rewind[1]].Executed)
+
+			//replace rewind location with replaced jmp/nop
+			if instructionsList[rewind[1]].Operation == "jmp" {
+				_arg := instructionsList[rewind[1]].Argument
+				instructionsList[rewind[1]] = instruction.New("nop", _arg)
+				index = rewind[1]
+				continue process
+			} else if instructionsList[rewind[1]].Operation == "nop" {
+				_arg := instructionsList[rewind[1]].Argument
+				instructionsList[rewind[1]] = instruction.New("jmp", _arg)
+				index = rewind[1]
+				continue process
+			} else {
+				fmt.Printf("operation is %s, not something we can fix", instructionsList[rewind[1]].Operation)
+				break process
+			}
 		} else {
 			instructionsList[index] = current.Exec()
 		}
@@ -73,7 +117,5 @@ process:
 		}
 
 	}
-
-	fmt.Printf("all done, accumulator is %d\n", accumulator)
-
+	fmt.Printf("program exiting, accumulator is %d\n", accumulator)
 }
