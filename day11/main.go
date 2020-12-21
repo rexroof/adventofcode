ad522110e8c7b1c5c4ce942c86fb0b77d482adae
Rex Roof <rexroof@gmail.com>
2020-12-21 09:51:16 -0500

    part one one!

--- day11/main.go
+++ day11/main.go
@@ -21,61 +21,170 @@ func PrintRow(row []rune) {
 	fmt.Println(string(row))
 }
 
-// scan whole array checking and applying rule1
-// If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-func RuleOne(seats [][]rune) [][]rune {
-	for x, row := range seats {
-		for y, s := range row {
-			if s == 'L' {
-				fmt.Printf("%d %d %c\n", x, y, s)
-				clear := true
-
-				// above
-				if x-1 >= 0 {
-					abv := seats[x-1][y]
-					if abv == '#' {
-						clear = false
-					}
-				}
-
-				// below
-				if x+1 < len(seats) {
-					blw := seats[x+1][y]
-					if blw == '#' {
-						clear = false
-					}
-				}
-
-				// left
-				if y-1 >= 0 {
-					lft := seats[x][y-1]
-					if lft == '#' {
-						clear = false
-					}
-				}
-
-				// right
-				if y+1 < len(seats[x]) {
-					rgt := seats[x][y+1]
-					if rgt == '#' {
-						clear = false
-					}
-				}
-
-				if clear == true {
-					seats[x][y] = '#'
-				}
-
+// return number of occupied seats
+func countOccupied(seats [][]rune) int {
+	_count := 0
+	for _, row := range seats {
+		for _, s := range row {
+			if s == '#' {
+				_count++
 			}
 		}
 	}
-	return seats
+	return _count
+}
+
+// returns a seat if it exists.  returns X if doesn't exist
+func grabSeat(seats [][]rune, x int, y int) rune {
+	_s := 'X'
+
+	if x >= 0 && y >= 0 {
+		if x < len(seats) && y < len(seats[x]) {
+			_s = seats[x][y]
+		}
+	}
+	return _s
 }
 
 // scan whole array checking and applying rule1
+// If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+func RuleOne(seats [][]rune) ([][]rune, bool) {
+	_copyseats := [][]rune{}
+	_changed := false
+
+	for x, row := range seats {
+		_copyrow := []rune{}
+		for y, s := range row {
+			if s == 'L' {
+				// fmt.Printf("%d %d %c\n", x, y, s)
+				clear := true
+
+				// above
+				if grabSeat(seats, x-1, y) == '#' {
+					clear = false
+				}
+
+				// above left
+				if grabSeat(seats, x-1, y-1) == '#' {
+					clear = false
+				}
+
+				// above right
+				if grabSeat(seats, x-1, y+1) == '#' {
+					clear = false
+				}
+
+				// below
+				if grabSeat(seats, x+1, y) == '#' {
+					clear = false
+				}
+
+				// below left
+				if grabSeat(seats, x+1, y-1) == '#' {
+					clear = false
+				}
+
+				// below right
+				if grabSeat(seats, x+1, y+1) == '#' {
+					clear = false
+				}
+
+				// left
+				if grabSeat(seats, x, y-1) == '#' {
+					clear = false
+				}
+
+				// right
+				if grabSeat(seats, x, y+1) == '#' {
+					clear = false
+				}
+
+				if clear == true {
+					_copyrow = append(_copyrow, '#')
+					_changed = true
+				} else {
+					_copyrow = append(_copyrow, s)
+				}
+			} else {
+				_copyrow = append(_copyrow, s)
+			}
+		}
+		_copyseats = append(_copyseats, _copyrow)
+	}
+	return _copyseats, _changed
+}
+
+// scan whole array checking and applying rule2
 // If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-func RuleTwo(seats [][]rune) [][]rune {
-	return seats
+func RuleTwo(seats [][]rune) ([][]rune, bool) {
+	_copyseats := [][]rune{}
+	_changed := false
+
+	for x, row := range seats {
+		_copyrow := []rune{}
+
+		for y, s := range row {
+			if s == '#' {
+				// fmt.Printf("%d %d %c\n", x, y, s)
+				neighbors := 0
+
+				// above
+				if grabSeat(seats, x-1, y) == '#' {
+					neighbors++
+				}
+
+				// above left
+				if grabSeat(seats, x-1, y-1) == '#' {
+					neighbors++
+				}
+
+				// above right
+				if grabSeat(seats, x-1, y+1) == '#' {
+					neighbors++
+				}
+
+				// below
+				if grabSeat(seats, x+1, y) == '#' {
+					neighbors++
+				}
+
+				// below left
+				if grabSeat(seats, x+1, y-1) == '#' {
+					neighbors++
+				}
+
+				// below right
+				if grabSeat(seats, x+1, y+1) == '#' {
+					neighbors++
+				}
+
+				// left
+				if grabSeat(seats, x, y-1) == '#' {
+					neighbors++
+				}
+
+				// right
+				if grabSeat(seats, x, y+1) == '#' {
+					neighbors++
+				}
+
+				if neighbors > 3 {
+					_copyrow = append(_copyrow, 'L')
+					// fmt.Printf("%d %d - changed to %c\n", x, y, s)
+					_changed = true
+				} else {
+					_copyrow = append(_copyrow, s)
+					// fmt.Printf("%d %d - kept with %c (%d) \n", x, y, s, neighbors)
+				}
+
+			} else {
+				_copyrow = append(_copyrow, s)
+			}
+		}
+
+		_copyseats = append(_copyseats, _copyrow)
+	}
+	return _copyseats, _changed
 }
 
 func main() {
@@ -100,11 +209,43 @@ func main() {
 		seats = append(seats, oneRow)
 	}
 
-	seats = RuleOne(seats)
+	fmt.Println("first few rows:")
+	PrintRow(seats[0])
+	PrintRow(seats[1])
+	PrintRow(seats[2])
+	PrintRow(seats[3])
+	PrintRow(seats[4])
 
-	PrintRow(seats[5])
-	PrintRow(seats[6])
-	PrintRow(seats[7])
-	PrintRow(seats[8])
+	// apply each rule
+	seats, _ = RuleOne(seats)
+	seats, _ = RuleTwo(seats)
+
+	// if we are still moving
+	stillShifting := true
+	// how many times we applied the rules
+	rulesCount := 2
+
+	for stillShifting == true {
+		one, two := false, false
+
+		seats, one = RuleOne(seats)
+		rulesCount++
+		seats, two = RuleTwo(seats)
+		rulesCount++
+
+		if one == false && two == false {
+			stillShifting = false
+		}
+
+	}
+
+	fmt.Printf("after %d runs of the rules: \n", rulesCount)
+	PrintRow(seats[0])
+	PrintRow(seats[1])
+	PrintRow(seats[2])
+	PrintRow(seats[3])
+	PrintRow(seats[4])
+
+	fmt.Printf("counted %d seats occupied\n", countOccupied(seats))
 
 }
